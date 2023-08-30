1f1f510e64e45b02c1a8a67271ba84ff0f45a113
Esmanur Karatas <83882274+esmanur-karatas@users.noreply.github.com>
2023-08-30 14:39:13 +0300

    Add files via upload

---
+++ YildizlarlaSekilOlusturma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YildizlarlaSekilOlusturma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    //Yıldızlarla Dikdörtgen Oluşturma
+            //    for (int i =1 ; i <=10; i++)
+            //    {
+            //        Console.WriteLine("********");            }
+            //}
+
+            // Yıldızlarla Dik Üçgen Yapma
+            for (int j = 1; j <= 5; j++)
+            {
+                for (int k = 1; k <= j; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
